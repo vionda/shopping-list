aa6ba8c21480435fe05df9262adb037c293cee83
vionda <viondasalihu@gmail.com>
2020-12-15 02:34:55 +0100

    Create product component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,81 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import data from "./data.json";
+import styled from "styled-components";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Products from "./components/Products";
+
+const Container = styled.div`
+  display: grid;
+  grid-template-areas: "header" "main" "footer";
+  grid-template-rows: 5rem 1fr 5rem;
+  grid-template-columns: 1fr;
+  height: 100%;
+`;
+const Header = styled.header`
+  grid-area: header;
+  background-color: #203040;
+  color: #ffffff;
+  display: flex;
+  align-items: center;
+  padding: 0.5rem;
+`;
+const Link = styled.a`
+  color: #ffffff;
+  text-decoration: none;
+  :hover {
+    color: #ff8000;
+  }
+`;
+const Main = styled.main`
+  grid-area: main;
+`;
+const Content = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+`;
+const Product = styled.div`
+  flex: 3 60rem;
+`;
+const Sidebar = styled.div`
+  flex: 1 20rem;
+`;
+
+const Footer = styled.footer`
+  grid-area: footer;
+  background-color: #203040;
+  color: #ffffff;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      products: data.products,
+      size: "",
+      sort: "",
+    };
+  }
+  render() {
+    return (
+      <Container>
+        <Header>
+          <Link href="/">React Shopping Cart</Link>
+        </Header>
+        <Main>
+          <Content>
+            <Product>
+              <Products products={this.state.products}></Products>
+            </Product>
+            <Sidebar>Sidebar</Sidebar>
+          </Content>
+        </Main>
+        <Footer>All right's reserved</Footer>
+      </Container>
+    );
+  }
 }
 
 export default App;
